a07589911fec3410c44b2df579ee791c696bb5bd
Andhika Guntur Slamet <andikaaddobe@gmail.com>
2025-12-13 18:53:03 +0700

    feat(routes): add migration and seeder routes
    fix(Api): update product image retrieval method
    
    fix(Product_model): order products by creation date in queries
    
    fix(index): update product image source path in view

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -597,7 +597,7 @@
             <div class="col-lg-3 col-md-4 col-sm-6">
                 <div class="product-card">
                     <div class="position-relative">
-                        <img src="${product.image}" 
+                        <img src="${BASE_URL}uploads/products/${product.image}" 
                             class="product-image" 
                             alt="${escapeHtml(product.product_name)}"
                             onerror="this.src='https://images.unsplash.com/photo-1598327105666-5b89351aff97?w=400'">
